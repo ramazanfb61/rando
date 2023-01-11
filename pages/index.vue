980fc9b338567cb7ba49a2e4f1956c66f7617c8f
ramazanfb61 <ramazanfb61@gmail.com>
2023-01-11 15:13:47 +0300

    done

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="">
-    <section class=" py-4 shadow-2xl rounded-lg md:w-1/2 mx-auto">
+    <section v-if="show" class=" py-4 shadow-2xl rounded-lg md:w-1/2 mx-auto border">
       <h2 class="italic text-lg font-medium">Choose an aspect or,</h2>
       <h2 class="italic text-lg font-medium">Let the fate guide you!</h2>
     </section>
-    <section class="px-3 py-8 rounded-lg mt-8 md:w-full lg:w-3/4 mx-auto shadow-2xl">
-
-      <div class=" py-5 grid gap-3 md:gap-x-10 grid-cols-2 grid-rows-2 md:w-2/3 mx-auto ">
-        <div @click="active === 1 ? active = 0 : active = 1" :class="{'bg-green-500':active === 1}"
+    <section class="px-3 py-8 rounded-lg mt-8 md:w-full lg:w-3/4 mx-auto shadow-2xl border">
+      <div 
+        v-if="!show "
+        class="h-30"
+      >
+        <p class="mb-3">
+          {{ randoo }}
+        </p>
+        <button
+          @click="reload"
+          class="border border-button py-2 px-4 md:px-10 rounded-md text-button hover:bg-header hover:text-font  transition-all duration-200 ">
+          return
+        </button>
+      </div>
+      <div v-if="show" class=" py-5 grid gap-3 md:gap-x-10 grid-cols-2 md:w-full lg:w-2/3 mx-auto">
+        <div @click="active === 1 ? active = 0 : active = 1" :class="{'bg-green-500 text-white':active === 1}"
           class="h-20 md:w-52 rounded-2xl cursor-pointer md:hover:shadow-2xl transition-shadow duration-200 border shadow-lg outline-green-400 outline  outline-2 ">
           <div class="h-full flex justify-center items-center">
 
             <div class=" basis-20">Icon</div>
             <div class="text-left basis-36">Nature</div>
-          
+
           </div>
         </div>
         <div
-          class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-red-300 outline  outline-1 "
-          :class="{ 'bg-red-500': active === 2 }" @click="active === 2 ? active = 0 : active = 2">
+          class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-red-300 outline  outline-2 select-none"
+          :class="{ 'bg-red-500 text-white ': active === 2 }" @click="active === 2 ? active = 0 : active = 2">
           <div class="h-full flex justify-center items-center">
 
             <div class=" basis-20">Icon</div>
@@ -27,8 +39,8 @@
           </div>
         </div>
         <div
-          class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-blue-300 outline  outline-1 "
-          :class="{ 'bg-blue-500': active === 3 }" @click="active === 3 ? active = 0 : active = 3">
+          class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-blue-300 outline  outline-2 select-none"
+          :class="{ 'bg-blue-500 text-white': active === 3 }" @click="active === 3 ? active = 0 : active = 3">
           <div class="h-full flex justify-center items-center">
 
             <div class=" basis-20">Icon</div>
@@ -37,8 +49,8 @@
           </div>
         </div>
         <div
-          class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-yellow-300 outline  outline-1 "
-          :class="{ 'bg-yellow-500': active === 4 }" @click="active === 4 ? active = 0 : active = 4">
+          class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-yellow-300 outline  outline-2 select-none"
+          :class="{ 'bg-yellow-500 text-white': active === 4 }" @click="active === 4 ? active = 0 : active = 4">
           <div class="h-full flex justify-center items-center">
 
             <div class=" basis-20">Icon</div>
@@ -46,7 +58,7 @@
 
           </div>
         </div>
-        <!-- <div class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-green-300 outline  outline-1 ">
+        <!-- <div class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-green-300 outline  outline-2 select-none">
           <div class="h-full flex justify-center items-center">
             
             <div class=" basis-20">Icon</div>
@@ -54,7 +66,7 @@
 
           </div>
         </div>
-        <div class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-green-300 outline  outline-1 ">
+        <div class="h-20 md:w-52 rounded-2xl cursor-pointer hover:shadow-2xl transition-shadow duration-200 border shadow-lg  outline-green-300 outline  outline-2 select-none">
           <div class="h-full flex justify-center items-center">
             
             <div class=" basis-20">Icon</div>
@@ -63,49 +75,40 @@
           </div>
         </div> -->
       </div>
-      <div class="py-3">
+      <div v-if="show" class="py-3">
         <button
-          class="border border-button py-3 px-5 md:px-10 rounded-md text-button hover:bg-button hover:text-font  transition-all duration-200">Randoo</button>
+          @click="randooData(active)"
+          class="border border-button py-3 px-5 md:px-10 rounded-md text-button hover:bg-header hover:text-font  transition-all duration-200 ">
+          Randoo
+        </button>
       </div>
     </section>
   </div>
 </template>
 
-<script >
-import { supabase } from "../db/index"
+<script setup>
+import { fetchData } from "../db/index"
+import { ref, onBeforeMount } from "vue"
 
 
-export default {
-  name: 'IndexPage',
-  data() {
-    return {
-      api: [],
-      active: 0,
-    }
-  },
-  async created() {
-    console.log(supabase);
-    await this.fetchData();
-
-  },
-  methods: {
-    async fetchData() {
+const randoo = ref();
+const active = ref(0);
+const show = ref(true);
 
 
-      let { data, error } = await supabase
-        .from('person')
-        .select('name')
-
-      console.log(data)
-      console.log("data", data[0].name)
-
-      data.forEach(element => {
-        this.api.push(element.name)
-      });
-    },
-
-
-
-  }
+async function randooData(val){
+  show.value = false ;
+  randoo.value = ""
+  const api = await fetchData(val)
+  
+  let random = Math.floor(Math.random() * api.length)
+  api[random]
+  randoo.value = Object.values(api[random]).toString() 
 }
+
+function reload (){
+  show.value = true;
+  active.value = 0;
+} 
+
 </script>
